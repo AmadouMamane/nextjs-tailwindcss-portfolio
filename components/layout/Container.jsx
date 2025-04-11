317e2437ffdcbef38637b99e9fceeb1d5c98329a
Amadou Mamane <amadou@Amadous-Mac-Studio.local>
2025-04-11 21:51:20 +0200

    Feat: reduced the margins on small screens

--- components/layout/Container.jsx
+++ components/layout/Container.jsx
@@ -1,7 +1,7 @@
 export default function Container({ children, isBlog = false, fullWidth = false }) {
   return (
-    <div className={`w-full ${fullWidth ? '' : 'px-10 sm:px-10 lg:px-14'}`}>
-      <div className={`mx-auto ${isBlog ? 'max-w-[1800px]' : fullWidth ? '' : 'max-w-7xl px-10 sm:px-10 lg:px-10'}`}>
+    <div className={`w-full ${fullWidth ? '' : 'px-3 sm:px-10 lg:px-14'}`}>
+      <div className={`mx-auto ${isBlog ? 'max-w-[1800px]' : fullWidth ? '' : 'max-w-7xl px-3 sm:px-10 lg:px-10'}`}>
         {children}
       </div>
     </div>
